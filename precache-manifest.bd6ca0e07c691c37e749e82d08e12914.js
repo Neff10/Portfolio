fa1d8054db33966a185bb9c2f3d5529bb80c2418
Neff10 <st.tunkara@gmail.com>
2019-04-30 13:00:59 +0100

    Updates

---
+++ precache-manifest.bd6ca0e07c691c37e749e82d08e12914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb8e79c737314ca378b4521288a30b9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "282dcecb31c3a4b268ad",
+    "url": "/portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "282dcecb31c3a4b268ad",
+    "url": "/portfolio/static/js/main.cc25776f.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
