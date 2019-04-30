df631b20b8f9fb89082e2cc6f4b6b6cd4cac475d
Neff10 <st.tunkara@gmail.com>
2019-04-30 16:08:47 +0100

    First version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import pageimage from './pageimage.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello World!!</div>
+    <div className='container' img src={pageimage} alt="Background" >
+      <div className='details'>
+        <div class='fullName'>
+          <h1>Suleman <br/> Tunkara</h1>
+          <p class='description'>Web Developer</p>
+        </div>
+      </div>
     </div>
   );
 }
