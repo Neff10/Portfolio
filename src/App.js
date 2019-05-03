9c0b34075b0f6c95381088abe22d1480354f7479
Neff10 <st.tunkara@gmail.com>
2019-05-03 15:05:30 +0100

    First day

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React from 'react';
 import './App.css';
-import pageimage from './pageimage.jpg';
+import 'font-awesome/css/font-awesome.min.css';
+
 
 function App() {
   return (
-    <div className='container' img src={pageimage} alt="Background" >
-      <div className='details'>
-        <div class='fullName'>
-          <h1>Suleman <br/> Tunkara</h1>
-          <p class='description'>Web Developer</p>
-        </div>
-      </div>
-    </div>
+    
+    <html>
+      <head>
+      </head>
+      <body>
+        <header>
+          <div className='menuButton'>
+            <div className='buttonLine'></div>
+            <div className='buttonLine'></div>
+            <div className='buttonLine'></div>
+          </div>
+          <nav className='menu'>
+            <div className='menuBranding'>
+              <div className='portrait'></div>
+            </div>
+            <ul className='menuNav'>
+              <li className='navItem'>
+                <a href='/' className='navLink'>
+                  Home
+          </a>
+              </li>
+              <li className='navItem'>
+                <a href='/' className='navLink'>
+                  About Me
+          </a>
+              </li>
+              <li className='navItem'>
+                <a href='/' className='navLink'>
+                  Portfolio
+          </a>
+              </li>
+              <li className='navItem'>
+                <a href='/' className='navLink'>
+                  Contact
+          </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main id='home'>
+          <h1 className='name'>
+            Suleman
+            <span className='surname'> Tunkara</span>
+          </h1>
+          <h2 className='description'>Pokemon Trainer</h2>
+          <div className='icons'>
+          <a href='#!'>
+            <i className="fa fa-linkedin-square fa-2x"></i>
+          </a>
+          <a href='#!'>
+            <i className="fa fa-github-square fa-2x"></i>
+          </a>
+          <a href='#!'>
+            <i className="fa fa fa fa-free-code-camp fa-2x"></i>
+          </a>
+          <a href='#!'>
+            <i className="fa fa fa-slack fa-2x"></i>
+          </a>
+          </div>
+        </main>
+        <script src='js/main.js'></script>
+      </body>
+    </html>
   );
 }
+
 
 export default App;
